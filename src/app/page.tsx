7e643bf9fe3606e036214d20ce205d2c3cbfd230
mswat5 <mswat14361@gmail.com>
2024-12-06 16:43:03 +0530

    past date font as black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { format } from "date-fns";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white p-4 max-w-lg mx-auto">
+    <div className="min-h-screen bg-white p-6 max-w-lg  mx-auto">
       <div className="space-y-8">
-        <div className="text-4xl font-bold">{format(new Date(), "EEEE")}</div>
+        <div className="text-4xl font-bold ">{format(new Date(), "EEEE")}</div>
         <DatePicker />
         <AddTodoDialog />
       </div>
--- src/components/calendar/date-picker.tsx
+++ src/components/calendar/date-picker.tsx
@@ -4,11 +4,12 @@ import { format, isToday } from "date-fns";
 
 export function DatePicker() {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center ">
       {Array.from({ length: 7 }).map((_, index) => {
         const date = new Date();
         date.setDate(date.getDate() - 3 + index);
         const isCurrentDay = isToday(date);
+        const isPastDate = date < new Date(new Date().setHours(0, 0, 0, 0));
 
         return (
           <div
@@ -16,11 +17,13 @@ export function DatePicker() {
             className={`text-center ${
               isCurrentDay
                 ? "bg-black text-white rounded-lg p-2"
+                : isPastDate
+                ? "text-black"
                 : "text-gray-400"
             }`}
           >
             <div className="text-sm">{format(date, "EEE").charAt(0)}</div>
-            <div className="font-bold">{format(date, "d")}</div>
+            <div className={`font-bold `}>{format(date, "d")}</div>
           </div>
         );
       })}
